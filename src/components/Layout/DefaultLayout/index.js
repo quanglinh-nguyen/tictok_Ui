882c410d225c63362fa79d26b0cd3c5d58a59966
Huu Quang Linh Nguyen <quanglinh.nguyen@rikai.technology>
2022-10-19 09:45:26 +0700

    fix dupplicate

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,5 +1,5 @@
-import Header from './Header';
-import Sidebar from './Sidebar';
+import Header from '~/components/Layout/components/Header';
+import Sidebar from '~/components/Layout/componentsSidebar';
 function DefaultLayout({children}) {
     return (
         <div>
--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,9 +1,9 @@
-import Header from './Header';
-function DefaultLayout({children}) {
+import Header from '~/components/Layout/components/Header';
+function DefaultLayout({ children }) {
     return (
         <div>
             <Header />
-            <div className='container'>
+            <div className="container">
                 <div className="content">{children}</div>
             </div>
         </div>
