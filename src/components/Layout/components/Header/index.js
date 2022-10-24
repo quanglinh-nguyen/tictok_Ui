1906940a5ae8881efeb06694478c2dda87d8d265
quang Linh <quanglinh.nguyen@rikai.technology>
2022-10-24 23:44:06 +0700

    push code fix header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -11,6 +11,7 @@ import {
     faSignOut,
 } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react';
+import { Link } from 'react-router-dom';
 import 'tippy.js/dist/tippy.css'; // optional
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -19,7 +20,7 @@ import Menu from '~/components/Popper/Menu';
 import { UploadIcon, InboxIcon, MessageIcon, SearchIcon } from '~/components/Icons';
 import Image from '~/components/Image'
 import Search from '../Search';
-
+import routesConfig from '~/config/routes';
 const cx = classNames.bind(styles);
 
 const MENU_ITEMS = [
@@ -101,7 +102,9 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
+                <Link to={routesConfig.home} className={cx('logo-link')}>
                 <img src={images.logo} alt="Tiktok" />
+                </Link>
                 {/* search */}
                 <Search/>
                 <div className={cx('action')}>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
+import routesConfig from '~/config/routes';
 // Layouts
-import { HeaderOnly } from "~/components/Layout";
-
+import { HeaderOnly } from '~/components/Layout';
 
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
@@ -8,14 +8,13 @@ import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
 
-
 // public routes
 const publicRoutes = [
-    { path: '/', component: Home },
-    { path: '/following', component: Following },
-    { path: '/@:nickname', component: Profile },
-    { path: '/upload', component: Upload, layout: HeaderOnly },
-    { path: '/search', component: Search, layout: null },
+    { path: routesConfig.home, component: Home },
+    { path: routesConfig.following, component: Following },
+    { path: routesConfig.profile, component: Profile },
+    { path: routesConfig.upload, component: Upload, layout: HeaderOnly },
+    { path: routesConfig.search, component: Search, layout: null },
 ];
 
 const privateRoutes = [];
