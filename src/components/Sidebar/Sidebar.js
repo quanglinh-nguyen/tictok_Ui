b616e4a32f4bb45d475c82841743e3d0f61fab0d
quang Linh <quanglinh.nguyen@rikai.technology>
2022-10-25 23:14:26 +0700

    UI side bar 1

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,17 +1,30 @@
-import styles from "./Sidebar.module.scss";
 import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
 import Menu, { MenuItem } from './Menu';
+import {
+    HomeIcon,
+    HomeActiveIcon,
+    UserGroupIcon,
+    UserGroupActiveIcon,
+    LiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons';
 import config from '~/config';
-import { HomeIcon, UserGroupIcon, LiveIcon } from "~/components/Icons";
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
+
 function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
             <Menu>
-                <MenuItem title="For Your" to={config.routes.home} icon={<HomeIcon />} />
-                <MenuItem title="Following" to={config.routes.following} icon={<UserGroupIcon />} />
-                <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} />
+                <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
             </Menu>
         </aside>
     );
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,7 @@ import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 // public routes
 const publicRoutes = [
@@ -15,6 +16,7 @@ const publicRoutes = [
     { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
+    { path: config.routes.live, component: Live},
 ];
 
 const privateRoutes = [];
